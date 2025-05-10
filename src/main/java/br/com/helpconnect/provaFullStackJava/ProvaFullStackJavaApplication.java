38ece102bb705e1592be0b6bbfd1b2c3d12f2189
Kevin Alec Neri Lazzarotto <kevin.lazzarotto@hotmail.com>
2025-05-10 00:47:56 -0300

    Atuaizacao

--- src/main/java/br/com/helpconnect/provaFullStackJava/ProvaFullStackJavaApplication.java
+++ src/main/java/br/com/helpconnect/provaFullStackJava/ProvaFullStackJavaApplication.java
@@ -8,6 +8,18 @@ public class ProvaFullStackJavaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProvaFullStackJavaApplication.class, args);
+		logMemoryStats();
 	}
+
+	private static void logMemoryStats() {
+        Runtime runtime = Runtime.getRuntime();
+        long maxMemory = runtime.maxMemory();
+        long allocatedMemory = runtime.totalMemory();
+        long freeMemory = runtime.freeMemory();
+
+        System.out.println("[MEMORY] Max: " + (maxMemory / 1024 / 1024) + " MB");
+        System.out.println("[MEMORY] Allocated: " + (allocatedMemory / 1024 / 1024) + " MB");
+        System.out.println("[MEMORY] Free: " + (freeMemory / 1024 / 1024) + " MB");
+    }
 
 }

911a0c83f6ff7d49fdf6b212524c9d37ce093f53
Kevin Alec Neri Lazzarotto <kevin.lazzarotto@hotmail.com>
2025-05-11 11:38:00 -0300

    Ajustes 8

--- src/main/java/br/com/helpconnect/provaFullStackJava/ProvaFullStackJavaApplication.java
+++ src/main/java/br/com/helpconnect/provaFullStackJava/ProvaFullStackJavaApplication.java
@@ -2,10 +2,8 @@ package br.com.helpconnect.provaFullStackJava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan("br.com.helpconnect.provaFullStackJava.model")
 public class ProvaFullStackJavaApplication {
 
 	public static void main(String[] args) {

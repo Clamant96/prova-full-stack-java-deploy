1d22f09975751068cd5a1cc5553460b5ae5bfad1
Kevin Alec Neri Lazzarotto <kevin.lazzarotto@hotmail.com>
2025-05-11 11:26:25 -0300

    Ajustes 6

--- src/main/java/br/com/helpconnect/provaFullStackJava/confg/JpaConfig.java
+++ src/main/java/br/com/helpconnect/provaFullStackJava/confg/JpaConfig.java
@@ -8,12 +8,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableJpaRepositories(
     basePackages = "br.com.helpconnect.provaFullStackJava.repository",
     entityManagerFactoryRef = "entityManagerFactory"
 )
+@EnableTransactionManagement
 public class JpaConfig {
 
     @Bean
